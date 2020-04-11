0872d54c8546fc950270e2269c9d9ce959f55684
aleksandrusov <wrkrltd.usov@gmail.com>
2020-04-11 18:31:51 +0300

    Cleared up some old navigation code

--- app/src/main/java/com/example/tuskmanager/SharedViewModel.kt
+++ app/src/main/java/com/example/tuskmanager/SharedViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.tuskmanager
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -21,32 +20,14 @@ class SharedViewModel constructor(
     private val categoryRepository: CategoryRepository
 ) : ViewModel() {
 
-    private val _screen = MutableLiveData<Screen>()
-    val screen: LiveData<Screen> = _screen
-
-    private val _selectedDate = MutableLiveData<String>()
-    val selectedDate: LiveData<String> = _selectedDate
-
-    private val _selectedTime = MutableLiveData<String>()
-    val selectedTime: LiveData<String> = _selectedTime
-
-    private val _toolBarText = MutableLiveData<ToolBarText>()
-    val toolBarText: LiveData<ToolBarText> = _toolBarText
-
     private val _allCategories = MutableLiveData<List<CategoryDomainModel>>()
     val allCategories: LiveData<List<CategoryDomainModel>> = _allCategories
 
-    private val _allTasks= MutableLiveData<List<TaskDomainModel>>()
+    private val _allTasks = MutableLiveData<List<TaskDomainModel>>()
     val allTasks: LiveData<List<TaskDomainModel>> = _allTasks
 
     private val _currentCategory = MutableLiveData<CategoryDomainModel>()
     val currentCategory: MutableLiveData<CategoryDomainModel> = _currentCategory
-
-    private val _newTaskTitle = MutableLiveData<String>()
-    val newTaskTitle: LiveData<String> = _newTaskTitle
-
-    private val _newTaskDescription = MutableLiveData<String>()
-    val newTaskDescription: LiveData<String> = _newTaskDescription
 
     private val _currentTask = MutableLiveData<TaskDomainModel>()
     val currentTask: LiveData<TaskDomainModel> = _currentTask
@@ -54,8 +35,6 @@ class SharedViewModel constructor(
     private val disposables = CompositeDisposable()
 
     init {
-        _screen.value = Screen.ALL_TASKS
-        _toolBarText.value = ToolBarText.ALL_TASKS
         _currentTask.value = TASK_ADD_NEW
 
         disposables.add(
@@ -74,32 +53,29 @@ class SharedViewModel constructor(
             taskRepository.getAllTasks()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ newAllTasks ->
-                    _allTasks.value = newAllTasks
-                },
-                    {
-                        it.printStackTrace()
-                    })
+                .subscribe(
+                    { newAllTasks -> _allTasks.value = newAllTasks },
+                    { it.printStackTrace() }
+                )
         )
     }
 
     fun addTask() {
-
         val myDate = _currentTask.value?.dateDue + _currentTask.value?.timeDue
         val sdf = SimpleDateFormat("dd.MM.yyyyHH:mm", Locale.ROOT)
         val date = sdf.parse(myDate)
         val millis: Long = date.time
 
-       val newRepoTask = TaskRepoModel(
-           uniqueTaskId = _currentTask.value?.id ?: 0,
-           title = _currentTask.value?.title ?: "",
-           category = _currentTask.value?.category ?: "",
-           categoryIcon = _currentTask.value?.categoryIcon ?: "",
-           color = _currentTask.value?.color ?: "",
-           dateAndTimeCreated = System.currentTimeMillis(),
-           dateAndTimeDue = millis,
-           description = _currentTask.value?.description ?: ""
-       )
+        val newRepoTask = TaskRepoModel(
+            uniqueTaskId = _currentTask.value?.id ?: 0,
+            title = _currentTask.value?.title ?: "",
+            category = _currentTask.value?.category ?: "",
+            categoryIcon = _currentTask.value?.categoryIcon ?: "",
+            color = _currentTask.value?.color ?: "",
+            dateAndTimeCreated = System.currentTimeMillis(),
+            dateAndTimeDue = millis,
+            description = _currentTask.value?.description ?: ""
+        )
 
         disposables.add(
             taskRepository.insertTask(newRepoTask)
@@ -112,12 +88,6 @@ class SharedViewModel constructor(
                     }
                 )
         )
-    }
-
-    fun fabShow(): Boolean {
-        return _currentTask.value?.dateDue != "" && _currentTask.value?.timeDue != "" &&
-                _currentTask.value?.title != "" && _currentTask.value?.description != ""
-                && _currentTask.value?.category != ""
     }
 
     fun onTaskClicked(newlySelected: TaskDomainModel) {
@@ -145,15 +115,9 @@ class SharedViewModel constructor(
         _currentTask.value?.category = newlySelected.title
         _currentTask.value?.categoryIcon = newlySelected.icon
         _currentTask.value?.color = newlySelected.color
-        _screen.value = Screen.NEW_TASK
-    }
-
-    fun onAddCategoryClicked() {
-        _screen.value = Screen.NEW_CATEGORY
     }
 
     fun addCategory() {
-
         val newCategory = CategoryRepoModel(
             id = 0L,
             title = "Werk",
@@ -175,56 +139,6 @@ class SharedViewModel constructor(
         )
     }
 
-    fun fabClicked() {
-        when (_screen.value) {
-            Screen.ALL_TASKS -> _screen.value = Screen.NEW_TASK
-            Screen.NEW_TASK -> {
-                _screen.value = Screen.ALL_TASKS
-                addTask()
-            }
-            Screen.NEW_CATEGORY -> _screen.value = Screen.NEW_TASK
-        }
-    }
-
-    fun chooseCategoryClicked() {
-        _screen.value = Screen.ALL_CATEGORIES
-    }
-
-    fun setToolBarText() {
-        val newToolBarText = when (_screen.value ?: return) {
-            Screen.ALL_TASKS -> ToolBarText.ALL_TASKS
-            Screen.NEW_TASK -> ToolBarText.NEW_TASK
-            Screen.ALL_CATEGORIES -> ToolBarText.CHOOSE_CATEGORY
-            Screen.NEW_CATEGORY -> ToolBarText.NEW_CATEGORY
-        }
-        _toolBarText.value = newToolBarText
-    }
-
-    fun onBackPressed() {
-        val newScreen = when (_screen.value ?: return) {
-            Screen.NEW_TASK -> Screen.ALL_TASKS
-            Screen.ALL_CATEGORIES -> Screen.NEW_TASK
-            Screen.NEW_CATEGORY -> Screen.ALL_CATEGORIES
-            else -> return
-        }
-        _screen.value = newScreen
-        if (_screen.value == Screen.ALL_TASKS) {
-            _currentCategory.value = CATEGORY_ADD_NEW
-            _currentTask.value = TASK_ADD_NEW
-        }
-    }
-
-    enum class Screen {
-        ALL_TASKS, NEW_TASK, ALL_CATEGORIES, NEW_CATEGORY
-    }
-
-    enum class ToolBarText(val displayableName: String) {
-        ALL_TASKS("All Tasks"),
-        NEW_TASK("New Task"),
-        CHOOSE_CATEGORY("Choose a category"),
-        NEW_CATEGORY("New Category")
-    }
-
     companion object {
         val CATEGORY_ADD_NEW = CategoryDomainModel(
             0,
@@ -233,9 +147,9 @@ class SharedViewModel constructor(
             "#179AB7"
         )
 
-        val TASK_ADD_NEW : TaskDomainModel
+        val TASK_ADD_NEW: TaskDomainModel
             get() {
-               return TaskDomainModel(
+                return TaskDomainModel(
                     0,
                     "",
                     "",
